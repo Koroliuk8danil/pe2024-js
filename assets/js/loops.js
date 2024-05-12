2cdad440a054f44e6b9282744059acee2ec83118
danol <danilkorolyuk017@gmail.com>
2024-05-12 17:28:10 +0200

    Фвв фссгь

--- assets/js/loops.js
+++ assets/js/loops.js
@@ -71,13 +71,57 @@
 //   console.log("Password is wrong");
 // }
 
-const RIGHT_PASSWORD = "qwerty";
-const TRY_LIMIT = 3;
-let isPasswordCorrect = false;
-let currentTry = 1;
-debugger;
-do {
-  isPasswordCorrect = prompt("Input password") === RIGHT_PASSWORD;
-} while (++currentTry <= TRY_LIMIT && !isPasswordCorrect);
+// const RIGHT_PASSWORD = "qwerty";
+// const TRY_LIMIT = 3;
+// let isPasswordCorrect = false;
+// let currentTry = 1;
+// debugger;
+// do {
+//   isPasswordCorrect = prompt("Input password") === RIGHT_PASSWORD;
+// } while (++currentTry <= TRY_LIMIT && !isPasswordCorrect);
 
-console.log(`Password is ${isPasswordCorrect ? "correct" : "wrong"}`);
+// console.log(`Password is ${isPasswordCorrect ? "correct" : "wrong"}`);
+
+// якщо тіло циклу має виконатися хоч 1 раз то do..while
+// інакше while
+
+// startPage = 1;
+// stopPage = 10;
+// step = 1;
+
+// for (let startPage = 1; startPage <= stopPage; startPage++) {
+//   console.log(startPage);
+// }
+
+// let startNumber = 101;
+// let endNumber = 91;
+// for (let i = startNumber; i >= endNumber; i--) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+// const startNumber = 1;
+// const endNumber = 3;
+// let sum = 0;
+
+// for (let i = startNumber; i <= endNumber; i++) {
+//   sum = sum + i;
+// }
+// console.log(sum);
+
+// 0+1 = 1
+// 1+2 = 3
+// 3+3 = 6
+
+// накопичити добуток парних чисел від 5 до 12 (6 8 10 12 = 5760)
+
+// const startNumber = 5;
+// const endNumber = 12;
+// let mul = 1;
+// for (let i = startNumber; i <= endNumber; i++) {
+//   if (i % 2 == 0) {
+//     mul *= i;
+//   }
+// }
+// console.log("Mul:", mul);
