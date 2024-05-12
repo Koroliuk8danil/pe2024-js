33f518713180e30b48a13cfe4bf502eb49a58a0c
danol <danilkorolyuk017@gmail.com>
2024-05-12 16:28:02 +0200

    Add do...while

--- assets/js/loops.js
+++ assets/js/loops.js
@@ -24,3 +24,60 @@
 //   }
 //   currentPage += 1;
 // }
+
+// let inputNumber = Number(prompt("Input number"));
+
+// while (!Number.isFinite(inputNumber)) {
+//   inputNumber = Number(prompt("Input number"));
+// }
+
+// console.log(`${inputNumber} ** 2 = ${inputNumber ** 2}`);
+
+// let inputNumber = null;
+
+// do {
+//   let inputNumber = Number(prompt("Input number"));
+// } while (!Number.isFinite(inputNumber));
+
+// console.log(`${inputNumber} ** 2 = ${inputNumber ** 2}`);
+
+// let startPage = 50;
+// const endPage = 40;
+
+// do {
+//   console.log("Current page: ", startPage--);
+// } while (startPage >= endPage);
+
+// Користувач має 3 спроби ввести пароль
+// Якщо пароль вірний, то вивести "Пароль вірний"
+// Інакше "Пароль невірний"
+
+// const RIGHT_PASSWORD = "qwerty";
+// const TRY_LIMIT = 3;
+// let isPasswordCorrect = false;
+
+// let currentTry = 1;
+
+// do {
+//   let inputPassword = prompt("Input password");
+//   if (inputPassword === RIGHT_PASSWORD) {
+//     console.log("Password is correct");
+//     isPasswordCorrect = true;
+//     break;
+//   }
+// } while (++currentTry <= TRY_LIMIT);
+
+// if (!isPasswordCorrect) {
+//   console.log("Password is wrong");
+// }
+
+const RIGHT_PASSWORD = "qwerty";
+const TRY_LIMIT = 3;
+let isPasswordCorrect = false;
+let currentTry = 1;
+debugger;
+do {
+  isPasswordCorrect = prompt("Input password") === RIGHT_PASSWORD;
+} while (++currentTry <= TRY_LIMIT && !isPasswordCorrect);
+
+console.log(`Password is ${isPasswordCorrect ? "correct" : "wrong"}`);
